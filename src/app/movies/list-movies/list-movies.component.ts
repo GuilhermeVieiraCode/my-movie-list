4879c6a35b134b552b96b796194245a8a6fd97ef
Guilherme Vieira <gui.vieira.code@gmail.com>
2023-05-29 13:39:35 -0300

    Create filters in movie list

--- src/app/movies/list-movies/list-movies.component.ts
+++ src/app/movies/list-movies/list-movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MoviesService } from 'src/app/core/movies.service';
 import { Movie } from 'src/app/shared/models/movie';
 
@@ -11,10 +12,31 @@ export class ListMoviesComponent implements OnInit{
   movies: Movie[] = [];
   pageNumber = 0;
   readonly quantityPerPage = 4;
+  filtersList!: FormGroup;
+  genres!: string[];
 
-  constructor(private moviesService: MoviesService) { }
+
+  constructor(private moviesService: MoviesService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.filtersList = this.formBuilder.group({
+      text: [''],
+      genre: ['']
+    });
+
+    this.genres = [
+    'Action', 
+    'Adventure', 
+    'Comedy', 
+    'Drama', 
+    'Fantasy', 
+    'Horror', 
+    'Romance', 
+    'Science Fiction', 
+    'Thriller', 
+    'Western'];
+
     this.listMovies();
   }
 
